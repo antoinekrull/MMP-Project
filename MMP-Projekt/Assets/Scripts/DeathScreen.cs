10498885be485979e5c9d531746416830f586b2e
Antoine Krull <antoine.krull@yahoo.de>
2022-07-14 11:33:57 +0200

    created DeathMenu surface

---
+++ MMP-Projekt/Assets/Scripts/DeathScreen.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathScreen : MonoBehaviour
+{
+    //SpawnController spawnController = SpawnController.GetInstance();
+    //int survivedWaves = spawnController.currentWaveNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
